b1557411dae6e19b539f68063cfe92d5eb77e91a
ciggy <86994699+RoleplayHacking@users.noreply.github.com>
2021-07-05 21:34:53 -0500

    Add files via upload

---
+++ nekoscommandsection.py
@@ -0,0 +1,595 @@
+@bot.command()
+async def lesbian(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/les")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def hentaiapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/Random_hentai_gif")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def meowapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/meow")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def erokapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/erok")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def lizardapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/lizard")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def feetgapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/feetg")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def bakaapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/baka")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def v3api(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/v3")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def bjapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/bj")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def erokemoapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/erokemo")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def tickleapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/tickle")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def feedapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/feed")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def nekoapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/neko")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def kuniapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/kuni")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def femdomapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/femdom")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def futanariapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/futanari")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def smallboobsapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/smallboobs")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def gooseapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/goose")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def nekov3oapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/nekoapi_v3.1")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def pokeapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/poke")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def trapapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/trap")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def patapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/pat")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def boobsapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/boobs")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def blowjobapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/blowjob")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def hentaiapi1(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/hentai")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def hololewdapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/hololewd")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def ngifapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/ngif")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def foxgirlapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/fox_girl")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def wallpaperapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/wallpaper")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def lewdkapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/lewdk")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def sologapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/solog")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def pussyapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/pussy")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def yuriapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/yuri")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def lewdkemoapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/lewdkemo")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def lewdapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/lewd")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def analapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/anal")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def pwankgapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/pwankg")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def nsfwapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/nsfw_avatar")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def eronapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/eron")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def kissapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/kiss")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def pussyjpgapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/pussy_jpg")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def woofapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/woof")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def hugapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/hug")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def ketaapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/keta")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def cuddleapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/cuddle")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def eroyuriapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/eroyuri")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def slapapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/slap")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def cum2api(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/cum_jpg")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def waifuapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/waifu")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def gecgapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/gecg")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def titsapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/tits")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def avatarapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/avatar")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def holoeroapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/holoero")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def classicapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/classic")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def kemmimiapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/kemonomimi")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def feetapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/feet")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def gasmapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/gasm")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def spankapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/spank")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def erofeetapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/erofeet")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def eroapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/ero")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def soloapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/solo")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def cumapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/cum")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def smugapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/smug")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def holoapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/holo")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def nsfwnekoapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/nsfw_neko_gif")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+async def eightbapi(ctx): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/8ball")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+
